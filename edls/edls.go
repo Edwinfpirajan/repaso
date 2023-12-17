799ba39f7a0fb7833fb91c2f8d8e5ab1bb8c19f0
Edwinfpirajan <efpa1998@hotmail.com>
2023-12-16 22:25:17 -0500

    first commit

---
+++ edls/edls.go
@@ -0,0 +1,45 @@
+package main
+
+import "time"
+
+//file types
+
+const (
+	fileRegular int = iota
+	fileDirectory
+	fileExecutable
+	fileCompress
+	fileImage
+	fileLink
+)
+
+// fyle extension
+const (
+	exe = ".exe"
+	deb = ".deb"
+	zip = ".zip"
+	gz  = ".gz"
+	tar = ".tar"
+	rar = ".rar"
+	png = ".png"
+	jpg = ".jpg"
+	gif = ".gif"
+)
+
+type file struct {
+	name             string
+	fileType         int
+	isDir            bool
+	isHidden         bool
+	userName         string
+	groupName        string
+	size             int64
+	modificationTime time.Time
+	mode             string
+}
+
+type styleFileType struct {
+	icon   string
+	color  string
+	symbol string
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Edwinfpirajan/repaso.git
+
+go 1.21.5
